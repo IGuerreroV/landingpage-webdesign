b891d02a99bc821383f1b02793d616a05dcfad22
Iván Guerrero <ivangvprogrammer@gmail.com>
2024-01-31 21:29:03 -0500

    [UPDATE] Agregando hover a los botones de menú y enlaces de contacto. Además, cambié la fuente de la letra a Roboto y corregí los errores ortográficos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
 import { useState } from 'react'
 import Dropdown from './Dropdown'
 // Icons
-import { RiAlignJustify, RiCloseFill } from 'react-icons/ri'
+import { RiAlignJustify, RiCloseFill, RiArrowRightLine } from 'react-icons/ri'
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false)
 
   return (
-    <div>
-      <div className='flex bg-[#B49F5B] justify-center font-semibold text-white text-[10p20px] w-full h-9 pt-1'>
-        <a>Agenda tu cita por Whatsap! Haz click AQUI!</a>
+    <div className='w-full'>
+      <div className='flex bg-[#B49F5B] justify-center font-semibold text-white w-full h-9 cursor-pointer hover:bg-[#c4ad65] transition duration-100 items-center gap-2'>
+        <a>Agenda tu cita por WhatsApp! Haz clic aquí</a>
+        <div className='absolute w-[99%] md:pl-[63%] pl-[91%] lg:pl-[66%] xl:pl-[920px] sm:pl-[75%] ease-in-out hover:translate-x-1 transition duration-100'>
+          <RiArrowRightLine />
+        </div>
       </div>
       <header className='flex xl:flex-col justify-between xl:justify-start items-center w-full xl:h-[44vh] h-[16vh] bg-[white] z-50 pt-2'>
         <div className='container flex flex-1 justify-center items-center pl-10 pt-10 xl:p-0'>
           <a href='#'>
-            <img src='/logo/Hero.png' className='xl:w-80 xl:h-80 w-40 h-55' />
+            <img src='logo/Hero.png' className='xl:w-80 xl:h-80 w-40 h-55' />
           </a>
         </div>
-        <nav className={`fixed bg-white w-[70%] md:w-[40%] xl:w-[19%] xl:h-auto rounded-lg h-full
+        <nav className={`fixed bg-white w-[70%] md:w-[40%] xl:w-[22%] xl:h-auto rounded-lg h-full
         ${
         showMenu ? 'left-0' : '-left-full'
         }
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,22 +5,26 @@ const Information = () => {
       <section>
         <div className='flex flex-col items-center justify-center '>
           <div className='flex flex-col'>
-            <button className='bg-[#B49F5B] text-white py-2 px-8 rounded-xl text-xl m-1'>Contactanos via WhatsApp</button>
-            <button className=' text-black py-2 px-8 rounded-xl text-xl border-gray-500 border-[2px] m-1'>
-              Linea de Atencion xxxxxxxxx
+            <button className='bg-[#B49F5B] text-white py-2 px-8 rounded-xl text-xl m-1 hover:scale-[102%] font-normal'>
+              Contáctanos por WhatsApp
+            </button>
+            <button className=' text-black py-2 px-8 rounded-xl text-xl border-gray-500 border-[2px] m-1 hover:border-black hover:scale-[101%] font-normal'>
+              Línea de Atención xxxxxxxxx
             </button>
           </div>
 
           <div className='items-center justify-center'>
-            <h1 className='text-center text-2xl font-semibold pb-6'>Nuestra Empresa</h1>
-            <p className='text-center text-gray-500 text-[20px] xl:text-xl xl:leading-[2.5rem] leading-8 xl:px-60 px-10'>En TRANSFORMAR estamos comprometidos con materializar los sueños de nuestros clientes, creando ESPACIOS únicos que reflejen la utilidad, buen gasto y comodidad. Con nuestro equipo asumimos el compromiso de diseño ejecución y entrega de cada proyecto.</p>
+            <h1 className='text-center text-2xl font-medium pb-6 pt-1 tracking-[1px]'>Nuestra Empresa</h1>
+            <p className='text-center text-gray-500 text-[20px] xl:text-xl xl:leading-[2.5rem] leading-8 xl:px-80 px-10 font-mono'>
+              En <span className='text-[#000]'>TRANSFORMAR</span> estamos comprometidos con materializar los sueños de nuestros clientes, creando ESPACIOS únicos que reflejen la utilidad, buen gasto y comodidad. Con nuestro equipo asumimos el compromiso de diseño ejecución y entrega de cada proyecto.
+            </p>
           </div>
 
         </div>
       </section>
       <section>
         <div>
-          <p className='font-bold xl:text-4xl p-5 xl:pl-60 pl-12'>Nuestros Proyectos</p>
+          <p className='font-bold xl:text-4xl p-5 xl:pl-60 pl-12 text-3xl tracking-wider'>Nuestros Proyectos</p>
         </div>
         <div className='grid gap-1 items-center justify-center xl:px-60 p-10 pb-20'>
           <div className='grid grid-cols-3 gap-1'>
@@ -41,12 +45,12 @@ const Information = () => {
 
           </div>
           <div>
-            <p className='font-bold text-4xl py-10 text-center'>Nuestros servcios</p>
+            <p className='font-bold text-4xl py-10 xl:text-center xl:tracking-widest tracking-wide'>Nuestros servicios</p>
           </div>
-          <div className='flex xl:gap-24 md:gap-20 gap-10 rounded-[10px] justify-center '>
-            <img className='p-3 bg-[#E7E4E4] xl:h-64 md:h-[150px] h-[70px]  ' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
-            <img className='p-3 bg-[#E7E4E4] rounded-[10px] xl:h-64 h-[70px] md:h-[150px]' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
-            <img className='p-3 bg-[#E7E4E4] rounded-[10px] xl:h-64 h-[70px] md:h-[150px]' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
+          <div className='flex flex-col xl:flex-row xl:gap-24 md:gap-5 gap-10 rounded-[10px] justify-center items-center'>
+            <img className='p-3 bg-[#E7E4E4] xl:h-64 md:h-[550px] h-[70%] w-full ' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
+            <img className='p-3 bg-[#E7E4E4] rounded-[10px] xl:h-64 h-[70%] md:h-[550px] w-full' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
+            <img className='p-3 bg-[#E7E4E4] rounded-[10px] xl:h-64 h-[70%] md:h-[550px] w-full' src='https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80' />
           </div>
         </div>
 
